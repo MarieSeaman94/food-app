5eb400eb33e7ac9a2042a2ccdbed53caaebb69a2
marie seaman <marie.seaman1202@gmail.com>
2022-11-17 23:51:08 +0900

    small change

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,13 +1,12 @@
 import { useEffect } from "react";
 
 function Popular() {
-
-useEffect(() => {
-  getPopular();
-},[]);
+  useEffect(() => {
+    getPopular();
+  },[]);
 
   const getPopular = async () => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}number=9`);
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
     const data = await api.json();
     console.log(data);
   };
