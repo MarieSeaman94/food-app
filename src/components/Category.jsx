39bf69982449b09a262553b0d5bd298f7625541f
marie seaman <marie.seaman1202@gmail.com>
2022-11-20 17:08:42 +0900

    Cuisine page added and routing added to Pages

---
+++ src/components/Category.jsx
@@ -0,0 +1,36 @@
+import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
+import { GiNoodles, GiChopsticks } from 'react-icons/gi';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+
+function Category() {
+  return (
+    <List>
+      <NavLink to={ '/cuisine/Italian' }>
+        <FaPizzaSlice />
+        <h4>Italian</h4>
+      </NavLink>
+      <NavLink to={ '/cuisine/American' }>
+        <FaHamburger />
+        <h4>American</h4>
+      </NavLink>
+      <NavLink to={ '/cuisine/Japanese' }>
+        <GiNoodles />
+        <h4>Japanese</h4>
+      </NavLink>
+      <NavLink to={ '/cuisine/Chinese' }>
+        <GiChopsticks />
+        <h4>Chinese</h4>
+      </NavLink>
+    </List>
+  );
+}
+
+const List = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0rem;
+`;
+
+export default Category;
