be769b43fdfcbf85a991b3fec853fac101dff310
marie seaman <marie.seaman1202@gmail.com>
2022-11-20 16:33:52 +0900

    new component of Category added to have buttons in the navbar

---
+++ src/components/Catergory.jsx
@@ -0,0 +1,18 @@
+import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
+import { GiNoodles, GiChopsticks } from 'react-icons/gi';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+import React from 'react'
+
+function Catergory() {
+  return (
+    <div>
+      <ul>
+
+      </ul>
+    </div>
+  )
+}
+
+export default Catergory;
