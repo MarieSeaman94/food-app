2aa018ced63ec32d5257434df11cf003648692ad
marie seaman <marie.seaman1202@gmail.com>
2022-11-26 22:11:26 +0900

    search file created and stylized

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -57,6 +57,19 @@ const StyledLink = styled(NavLink)`
     font-size: 1.5rem;
   }
 
+  &.active{
+    background: linear-gradient(to right, #f27121, #e94057);
+
+    svg{
+      color: white;
+
+    }
+
+    h4 {
+      color: white;
+    }
+  }
+
 `;
 
 
