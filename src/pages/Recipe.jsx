8ce118b740a76b9d4299b95cbec1b2f7bc24f5c8
marie seaman <marie.seaman1202@gmail.com>
2022-11-28 00:06:32 +0900

    activeTab added

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -34,10 +34,21 @@ function Recipe() {
          >
           Ingredients
           </Buttons>
-          <div>
-            <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
-            <h3 dangerouslySetInnerHTML={{ __html:details.instructions }}></h3>
+          {activeTab === 'instructions' && (
+            <div>
+              <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+              <h3 dangerouslySetInnerHTML={{ __html:details.instructions }}></h3>
           </div>
+          )}
+
+          {activeTab === 'ingredients' && (
+            <ul>
+              {details.extendedIngredients.map((ingredients) =>
+                <li key={ingredient.id}>{ingredient.original}</li>
+            )}
+          </ul>
+          )}
+
       </Info>
     </DetailWrapper>
   );
